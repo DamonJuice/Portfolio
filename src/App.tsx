5a4a60c120fa0671b5addb2844fcb5c14d190fdb
damonjuice <damonstangel@yahoo.com>
2025-02-14 19:59:50 -0600

    Updated majority of the files for more styling and link updates. Added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,83 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
 import './App.css'
 
+const images = [
+  "src/assets/icon.jpg",
+  "src/assets/oldIcon.png",
+  "src/assets/bsuFlowers.jpg", 
+  "src/assets/famPic.png"
+];
+
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Changes image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-    <h2>Powered By: Vite + React + TypeScript</h2>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    {/* INTRO SECTION */}
+<section id="intro" className="section">
+  <h1 className='introHeader'>Intro</h1>
+  <div className="introContainer">
+    <div className='introSection'>
+      <p>
+        Hello, my name is Damon Stangel. I'm currently an Application Developer for <a href='https://www.prowestgis.com/' target="_blank"> Pro-West & Associates Inc</a>. 
+        I consider myself a skilled front-end developer and application developer, who is passionate about building interactive web experiences.
+      </p>
+      &nbsp;
+      &nbsp;
+      <p className="skillsSection">
+        My skills include: 
+        <div className="listSkills">
+        <ul>
+          <li>TypeScript</li>
+          <li>Python</li>
+          <li>Angular</li>
+          <li>mySQL</li>
+        </ul>
+        <ul>
+          <li>Esri Javascript 4x API</li>
+          <li>.NET</li>
+          <li>React</li>
+          <li>C#</li>
+        </ul>
+        <ul>
+          <li>Javascript</li>
+          <li>OpenSearch API</li>
+          <li>Vite</li>
+          <li>AWS</li>
+        </ul>
+        </div>
+      </p>
+    </div>
+    <div className="slideshow-wrapper">
+      <img
+        src={images[currentIndex]}
+        className="active-slide"
+        alt="Slideshow"
+      />
+    </div>
+  </div>
+</section>
+
+{/* ABOUT ME SECTION */}
+
+      <section id="about" className="section">
+        <h1 className='aboutHeader'>About Me</h1>
+      </section>
       
+
+{/* PROJECTS SECTION */}
+      <section id="projects" className="section">
+        <h1 className='projectsHeader'>Projects</h1>
+      </section>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,24 +7,24 @@ import App from './App.tsx'
 
 function Links() {
   return (
-    
-<nav className="nav-bar">
-  {/* <a 
-    href="https://www.linkedin.com/in/damon-stangel-80155626b/" 
-    className="text-xl font-bold" 
-    target="_blank" 
-    rel="noopener noreferrer"
-  >
-    Damon Stangel
-  </a> */}
-  <div className="flex space-x-6">
-    <a href="/" className="nav-link">Home</a>
-    <a href="/about" className="nav-link">About Me</a>
-    <a href="/projects" className="nav-link">Projects</a>
-  </div>
-</nav>
+    <nav className="nav-bar">
+      <div className="flex space-x-6">
+        <a href="#intro" className="nav-link">Intro</a>
+        <a href="#about" className="nav-link">About Me</a>
+        <a href="#projects" className="nav-link">Projects</a>
+      </div>
+
+      {/* Move myLinks outside the flex container */}
+      <div className='myLinks'>
+        <a className='igLogo' href="https://www.instagram.com/damonjuice/"> <img src="src/assets/IG LOGO.png" alt="InstaLogo" /> </a>
+        <a className='linkedLogo' href="https://www.linkedin.com/in/damon-stangel-80155626b/"> <img src="src/assets/linkedIn logo.png" alt="LinkedIn Logo"></img> </a>
+        <a className='gitLogo' href="https://github.com/DamonJuice"> <img src="src/assets/git logo.png" alt="GitHub logo"></img> </a>
+      </div>
+    </nav>
   );
 }
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -32,3 +32,5 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
