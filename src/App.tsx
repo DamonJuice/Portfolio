82bb12469502407c725309de9e7a5f3262278476
damonjuice <damonstangel@yahoo.com>
2025-02-15 02:29:26 -0600

    Updated layout of form and fixed the browser deployment issues. Navbar updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from "react";
 import './App.css'
 
@@ -7,6 +6,12 @@ const images = [
   "src/assets/oldIcon.png",
   "src/assets/bsuFlowers.jpg", 
   "src/assets/famPic.png"
+];
+
+const projectLinks = [
+  { url: "https://github.com/yourusername/project1", image: "src/assets/famPic.png" },
+  { url: "https://github.com/yourusername/project2", image: "path/to/your/image2.jpg" },
+  { url: "https://github.com/yourusername/project3", image: "path/to/your/image3.jpg" },
 ];
 
 function App() {
@@ -20,71 +25,78 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+
+
   return (
     <>
-    {/* INTRO SECTION */}
-<section id="intro" className="section">
-  <h1 className='introHeader'>Intro</h1>
-  <div className="introContainer">
-    <div className='introSection'>
-      <p>
-        Hello, my name is Damon Stangel. I'm currently an Application Developer for <a href='https://www.prowestgis.com/' target="_blank"> Pro-West & Associates Inc</a>. 
-        I consider myself a skilled front-end developer and application developer, who is passionate about building interactive web experiences.
-      </p>
-      <p>Download my resume!</p>
-      <button>Download PDF</button>
-      &nbsp;
-      <p className="skillsSection">
-        My skills include: 
-        <div className="listSkills">
-        <ul>
-          <li>TypeScript</li>
-          <li>Python</li>
-          <li>Angular</li>
-          <li>mySQL</li>
-        </ul>
-        <ul>
-          <li>Esri Javascript 4x API</li>
-          <li>.NET</li>
-          <li>React</li>
-          <li>C#</li>
-        </ul>
-        <ul>
-          <li>Javascript</li>
-          <li>OpenSearch API</li>
-          <li>Vite</li>
-          <li>AWS</li>
-        </ul>
+      {/* INTRO SECTION */}
+      <section id="intro" className="section">
+        <h1 className='introHeader'>Intro</h1>
+        <div className="introContainer">
+          <div className='introSection'>
+            <p>
+              Hello, my name is Damon Stangel. I'm currently an Application Developer for <a href='https://www.prowestgis.com/' target="_blank"> Pro-West & Associates Inc</a>. 
+              I consider myself a skilled front-end developer and application developer, who is passionate about building interactive web experiences.
+            </p>
+            <p>Download my resume!</p>
+            <a href="public/resume.pdf" download="Damon Stangel Resume.pdf">
+            <button>Download PDF</button></a>
+            &nbsp;
+            <p className="skillsSection">
+              My skills include: 
+              <div className="listSkills">
+                <ul>
+                  <li>TypeScript</li>
+                  <li>Python</li>
+                  <li>Angular</li>
+                  <li>mySQL</li>
+                  <li>Node.js</li>
+                </ul>
+                <ul>
+                  <li>Esri Javascript 4x API</li>
+                  <li>.NET</li>
+                  <li>React</li>
+                  <li>C#</li>
+                </ul>
+                <ul>
+                  <li>Javascript</li>
+                  <li>OpenSearch API</li>
+                  <li>Vite</li>
+                  <li>AWS</li>
+                </ul>
+              </div>
+            </p>
+          </div>
+          <div className="slideshow-wrapper">
+            <img
+              src={images[currentIndex]}
+              className="active-slide"
+              alt="Slideshow"
+            />
+          </div>
         </div>
-      </p>
-    </div>
-    <div className="slideshow-wrapper">
-      <img
-        src={images[currentIndex]}
-        className="active-slide"
-        alt="Slideshow"
-      />
-    </div>
-    {/* Put the social media links underneath the image slideshow */}
-            {/* <a className='igLogo' href="https://www.instagram.com/damonjuice/" target="_blank"> <img src="src/assets/IG LOGO.png" alt="InstaLogo" /> </a>
-            <a className='linkedLogo' href="https://www.linkedin.com/in/damon-stangel-80155626b/" target="_blank"> <img src="src/assets/linkedIn logo.png" alt="LinkedIn Logo"></img> </a>
-            <a className='gitLogo' href="https://github.com/DamonJuice" target="_blank"> <img src="src/assets/git logo.png" alt="GitHub logo"></img> </a> */}
-  </div>
-</section>
-
-{/* ABOUT ME SECTION */}
-
-      <section id="about" className="section">
-        <h1 className='aboutHeader'>About Me</h1>
       </section>
-      
 
-{/* PROJECTS SECTION */}
+      {/* PROJECTS SECTION */}
       <section id="projects" className="section">
         <h1 className='projectsHeader'>Projects</h1>
+        <div className="projectsContainer">
+          <div className="projectsGrid">
+            {projectLinks.map((project, index) => (
+              <a href={project.url} key={index} className="projectItem" target="_blank" rel="noopener noreferrer">
+                <div
+                  className="projectImage"
+                  style={{ backgroundImage: `url(${project.image})` }}
+                >
+                  <span>Project {index + 1}</span>
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,63 @@
-/* eslint-disable react-refresh/only-export-components */
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-
 function Links() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className="nav-bar">
-      <div className="flex space-x-6">
-        <a href="#intro" className="nav-link">Intro</a>
-        <a href="#about" className="nav-link">About Me</a>
-        <a href="#projects" className="nav-link">Projects</a>
+      <div className="nav-container">
+        <div className="nav-links">
+          <a href="#intro" className="nav-link">Intro</a>
+          <a href="#about" className="nav-link">About Me</a>
+          <a href="#projects" className="nav-link">Projects</a>
+        </div>
+
+        <a className='nameTIME'>Damon Stangel</a>
+
+        {/* Hamburger menu (three lines) */}
+        <button className="hamburger" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        {/* Dropdown menu */}
+        {isMenuOpen && (
+          <div className="dropdown">
+            <a href="#intro" className="dropdown-link">Intro</a>
+            <a href="#about" className="dropdown-link">About Me</a>
+            <a href="#projects" className="dropdown-link">Projects</a>
+          </div>
+        )}
+
 
       </div>
     </nav>
   );
 }
 
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Links />
     <App />
-  </StrictMode>,
+    <footer>        
+      {/* Social media icons */}
+      <p>© 2025 Damon Stangel </p>
+        <a className="igLogo" href="https://www.instagram.com/damonjuice/" target="_blank">
+          <img src="src/assets/IG LOGO.png" alt="Instagram Logo" />
+        </a>
+        <a className="linkedLogo" href="https://www.linkedin.com/in/damon-stangel-80155626b/" target="_blank">
+          <img src="src/assets/linkedIn logo.png" alt="LinkedIn Logo" />
+        </a>
+        <a className="gitLogo" href="https://github.com/DamonJuice" target="_blank">
+          <img src="src/assets/git logo.png" alt="GitHub logo" />
+        </a></footer>
+  </StrictMode>
 )
-
-
